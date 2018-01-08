4219e85ecec8c59cfe1521ee02bfeacb7847d44e
Arnab Das <arnab.das682@gmail.com>
2018-01-08 21:21:17 +0600

    08-01-2018 1st day

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+	$fonts = arival;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Syntax</title>
+	<style>
+		.phpcoding{width:900px; margin: 0 auto;background: #ddd; }
+		
+		.headeroption,
+	    .footeroption{background:#444;color:#fff;text-align:center; padding: 20px;}
+		.headeroption h2, .footeroption h2{margin: 0;}
+		.maincontent{min-height: 400px;padding: 20px;}
+	
+	</style>
+</head>
+<body>
+
+<div class="phpcoding">
+	<section class="headeroption">
+		<h2><?php echo"PHP Fundamentals Training"; ?></h2>
+	</section>
+	
+	<section class="maincontent">
+		<?php
+			echo "I am Jmi...\n<br>";
+			echo $fonts;
+		?>
+	</section>
+	<section class="footeroption">
+		<h2><?php echo"www.jmidar-info.ml"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
+
+
+
+
+
+
