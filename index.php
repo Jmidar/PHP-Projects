ec8caa0667d48f5ae0d6be6027d4b957e52a8cef
Arnab Das <arnab.das682@gmail.com>
2018-01-13 03:05:51 +0600

    12-01-2017 4th Day

--- index.php
+++ index.php
@@ -2,6 +2,43 @@
 	$fonts = "verdana";
 	$bgcolor = "#444";
 	$fontcolor = "#FC8C41";
+	$errname = $erremail = $errweb = $errgender = "";
+	   $name = $email = $website = $comment = $gender = "";
+			if($_SERVER["REQUEST_METHOD"]=="POST"){
+			  if(empty($_POST["name"])){
+				$errname = "<span style='color:red'>Name is required</span>";
+			    }else{
+				   $name = validate($_POST["name"]);
+			       }
+			  if(empty($_POST["email"])){
+				 $erremail = "<span style='color:red'>Email is required</span>";
+			    }elseif(!filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)){
+				 $erremail = "<span style='color:red'>Invalid Email Formate!</span>";	
+				}else{
+					$email = validate($_POST["email"]);
+				    }
+			  if(empty($_POST["website"])){
+				 $errweb = "<span style='color:red'>Website is required</span>";
+			    }elseif(!filter_var($_POST["website"],FILTER_VALIDATE_URL)){
+				 $errweb = "<span style='color:red'>Invalid Website Formate!</span>";	
+				}else{
+					$website = validate($_POST["website"]);
+				    }
+			  if(empty($_POST["gender"])){
+				 $errgender = "<span style='color:red'>Gender is required</span>";
+			    }else{
+					$errgender = validate($_POST["gender"]);
+				    }
+				$comment = validate($_POST["comment"]);
+			  
+			}
+			function validate($data){
+				$data = trim($data);
+				$data = stripcslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+			}
+			
 ?>
 
 <!DOCTYPE html>
@@ -25,26 +62,47 @@
 	<section class="headeroption">
 		<h2><?php echo"PHP Fundamentals Training"; ?></h2>
 	</section>
-	PHP Sorting Array<hr>
+	PHP Form Validation<hr>
 	
 	<section class="maincontent">
-		<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-			Username:<input type="text" name="username" />
-			<input type="submit" name="Submit" />
-		</form>
-		<?php
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" >
+		<table>
 		
-			if($_SERVER["REQUEST_METHOD"]=="POST"){
-				$name = $_POST['username'];
-				if(empty($name)){
-					echo "<spam style='color:red'>Username field must not be empty !</spam>";
-				}
-				else{
-					echo "<spam style='color:green'>You have submitted: ".$name."</spam>";
-				}
-			}
-			
-		?>
+		<p style="color: red">* Required field</p>
+			<tr>
+				<td>Name:</td>
+				<td><input type = "text" name = "name"/>*<?php echo $errname;?></td>
+			</tr>
+			<tr>
+				<td>E-mail </td>
+				<td><input type = "text" name = "email"/>*<?php echo $erremail;?></td>
+			</tr>
+			<tr>
+				<td>Website </td>
+				<td><input type = "text" name = "website"/>*<?php echo $errweb;?></td>
+			</tr>
+			<tr>
+				<td>Comment </td>
+				<td><textarea name="comment"rows="5"cols="40"   ></textarea></td>
+			</tr>
+			<tr>
+				<td>Gender </td>
+				<td><input type = "radio" name = "gender" value="female" />Female
+				<input type = "radio" name = "gender" value="female" />male
+				*<?php echo $errgender;?></td>
+			</tr>
+			<tr></tr>			
+			<tr>
+				<td></td>
+				<td><input type = "submit" name = "Submit" value="Submit" /></td>
+			</tr>
+		</table>
+		</form>
+	<?php
+		echo date("Y/m/d")."<br>";
+		echo date("l");
+	?>
+	
 	</section>
 	<section class="footeroption">
 		<h2><?php echo"www.jmidar-info.ml"; ?></h2>
