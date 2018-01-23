aed67e3410398dc795ca34394f88d32f0d9cf689
Arnab Das <arnab.das682@gmail.com>
2018-01-23 21:01:35 +0600

    Full form JS 23/01/2018

--- index.php
+++ index.php
@@ -1,38 +1,98 @@
 <?php include("header.php")?>
 
 <script>
-	function checkFunction(){
-		var length = document.myform.coder.length;
-		$result = "";
-		for(i=0; i<length; i++){
-			
-			var checkValue = document.myform.coder[i].checked;
-			if(checkValue){
-				$result += document.myform.coder[i].value+", ";
+	function clickHere(){
+		var getname = document.myform.name.value;
+		document.getElementById('showName').innerHTML = getname;
+		
+		var genderLength = document.myform.gender.length;
+		for(i=0;i<genderLength;i++){
+			var checkGender = document.myform.gender[i].checked;
+			if(checkGender){
+				gendValue=document.myform.gender[i].value;
 			}
 		}
-		var showData = "You Selected : "+$result;
-		document.getElementById('show').innerHTML = showData;
+		document.getElementById('showGender').innerHTML = gendValue;
+		
+		var depLength = document.myform.dep.length;
+		for(i=0;i<depLength;i++){
+			var checkDep = document.myform.dep[i].checked;
+			if(checkDep){
+				dapValue = document.myform.dep[i].value;
+			}
+		}
+		document.getElementById('showDep').innerHTML = dapValue;
+		
+		var index = document.myform.coder.selectedIndex;
+		var coderValue = document.myform.coder.options[index].value;
+		document.getElementById('showCoder').innerHTML = coderValue;
 	}
 </script>
+<table class="tblone">
+	<tr>
+		<td colspan="2" align="center">Output</td>
+	</tr>
+	
+	<tr>
+		<td>Name: </td>
+		<td><span id="showName"></span></td>
+	</tr>
+	
+	<tr>
+		<td>Gender: </td>
+		<td><span id="showGender"></span></td>
+	</tr>
+	
+	<tr>
+	
+		<td>Department: </td>
+		<td><span id="showDep"></span></td>
+	</tr>
+	
+	<tr>
+		<td>Coder: </td>
+		<td><span id="showCoder"></span></td>
+	</tr>
+</table>
 
-<form action="index.php"  name="myform" id = "myform" onsubmit="checkFunction(); return false;" >
+<form action=""  name="myform" id = "myform" onsubmit = "clickHere(); return false;" >
 	<table>
 		<tr>
-			<td>Programming Language :<td>
-			<td><input type="checkbox" name="coder" value="PHP" />PHP<td>
-			<td><input type="checkbox" name="coder" value="Java" />Java<td>
-			<td><input type="checkbox" name="coder" value="C#" />C#<td>
-			
+			<td>Name: </td>
+			<td><input type="text" name="name" required="1" /></td>	
 		</tr>
 		<tr>
-			<td><td>
+			<td>Gender: </td>
+			<td><input type="radio" name="gender" value="Male" />Male	
+			<input type="radio" name="gender" value="Female" />Female</td>	
+		</tr>
+		<tr>
+			<td>Department: </td>
+			<td>
+				<input type="checkbox" name="dep" value="CSE" />CSE
+				<input type="checkbox" name="English" value="CSE" />English
+				<input type="checkbox" name="dep" value="LLB" />LLB
+			</td>	
+		</tr>
+		<tr>
+			<td>Coder: </td>
+			<td>
+				<select name="coder" requierd="1">
+					<option value="">Selected One</option>
+					<option value="JAVA">JAVA</option>
+					<option value="PHP">PHP</option>
+					<option value="JS">JS</option>
+					<option value="C#">C#</option>
+				</select>
+			</td>	
+		</tr>
+		
+		<tr>
+			<td></td>
 			<td><input type="submit" name="submit" value="Submit" /><input type="reset" value="Clear" /><td>
 		</tr>
 	</table>
 </form>
-<div id="show"></div>
-
 
 
 <?php include("footer.php")?>
